b4a0c329e898bea5bee7fd87cfa7a10b1684747b
Sebastián Batista Ferrera <sebastianbatistaferrera@gmail.com>
2024-11-16 18:46:30 -0400

    changes

--- src/core/users/application/user-services.ts
+++ src/core/users/application/user-services.ts
@@ -15,7 +15,6 @@ export class UserServices{
     }
 
     async loginUser(email: string, password: string): Promise <{token: string}>{
-
         const user = await this.userRepository.findUserByEmail(email);
         if(!user){
             throw "Usuario no encontrado"
@@ -31,7 +30,6 @@ export class UserServices{
     }
 
     async updateUser(id: number, updateUserDto: Partial<UpdateUserDto>): Promise < UserDto | null>{
-
         if(updateUserDto.password){           
             updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10)
         }  
@@ -39,15 +37,12 @@ export class UserServices{
         return this.userRepository.update(id, updateUserDto);
     }
 //
-    async forgotPassword(email: string): Promise <void>{
-
+    async forgotPassword(email: string): Promise <void>{       
         const token = crypto.randomBytes(32).toString("hex");       
         return this.userRepository.forgotPassword(email, token);
-
     }
 
     async resetPassword(id:number, {token, newPassword, passwordConfirmation}: ResetPasswordDto): Promise < UserDto | null>{
-
         const user = this.userRepository.findUserByToken(token);
         if(!user) return null; 
 
@@ -63,7 +58,6 @@ export class UserServices{
     }
 
     async getUser(token: string): Promise <UserDto | null>{ 
-
         const { email } = jwt.verify(token,process.env.TOKEN_SECRET) as {email: string}                   
         const user = await this.userRepository.findUserByEmail(email)               
         return user
