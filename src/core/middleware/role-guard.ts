2a429400a5115892c607a617b5a6646b72e3718e
Sebastián Batista Ferrera <sebastianbatistaferrera@gmail.com>
2024-12-18 15:27:23 -0400

    guard done

--- src/core/middleware/role-guard.ts
+++ src/core/middleware/role-guard.ts
@@ -6,12 +6,22 @@ import { Roles } from 'src/utility/roles-decorator';
 @Injectable()
 export class RolesGuard implements CanActivate{
     constructor(private reflector: Reflector){}
-    canActivate(context: ExecutionContext): boolean {
-        const roles = this.reflector.get(Roles, context.getHandler)
+    canActivate(
+        context: ExecutionContext
+    ): boolean | Promise<boolean>| Observable<boolean> {
+        const roles = this.reflector.get(Roles, context.getHandler)     
         if(!roles){
             return false;
-        }
-        //TO DO: Implementar lógica de validación de roles
+        }      
+
+        const request = context.switchToHttp().getRequest();
+        const user = request.user;
+
+        if (!user || !user.role) {         
+            return false;
+        }   
+        const isValidRole = [1, 2, 3].includes(user.role);      
+        return isValidRole;   
     }
 }
 
